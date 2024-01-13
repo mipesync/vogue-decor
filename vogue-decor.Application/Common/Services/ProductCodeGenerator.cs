7f45bd2f53d910bf68c8be0cfd416dd7d1d97c0a
Аркадий Алфёров <arhe414@gmail.com>
2024-01-13 18:34:39 +0300

    feature: Реализовал генератор кода товара

---
+++ vogue-decor.Application/Common/Services/ProductCodeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using vogue_decor.Application.Interfaces;
+
+namespace vogue_decor.Application.Common.Services;
+
+/// <summary>
+/// Генератор кода товара
+/// </summary>
+public class ProductCodeGenerator : IProductCodeGenerator
+{
+    public string GenerateCode(string name, string brand, string publicationDate, string type, string article, string color,
+        int length = 8)
+    {
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(brand) || string.IsNullOrEmpty(article) || string.IsNullOrEmpty(color))
+        {
+            throw new ArgumentException("Все параметры должны быть непустыми строками");
+        }
+        if (length is < 6 or > 10)
+        {
+            throw new ArgumentException("Длина кода товара должна быть от 6 до 10 цифр");
+        }
+
+        var input = $"{name},{brand},{publicationDate},{type},{article},{color}";
+
+        var hash = input.GetHashCode();
+
+        var code = Math.Abs(hash).ToString();
+
+        if (code.Length > length)
+        {
+            return code[..length];
+        }
+
+        return code.Length < length ? code.PadLeft(length, '0') : code;
+    }
+}
---
+++ vogue-decor.Application/Interfaces/IProductCodeGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace vogue_decor.Application.Interfaces;
+
+/// <summary>
+/// Интрейфейс для генератора кода
+/// </summary>
+public interface IProductCodeGenerator
+{
+    /// <summary>
+    /// Сгенерировать код товара
+    /// </summary>
+    /// <param name="name">Название товара</param>
+    /// <param name="brand">Название бренда</param>
+    /// <param name="publicationDate">Дата публикации товара</param>
+    /// <param name="type">Тип товара</param>
+    /// <param name="article">Артикул товара</param>
+    /// <param name="color">Цвет товара</param>
+    /// <param name="length">Длина генерируемого кода</param>
+    /// <returns>Код товара</returns>
+    public string GenerateCode(string name, string brand, string publicationDate, string type, string article, string color, int length = 8);
+}
